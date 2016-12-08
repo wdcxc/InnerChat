3feac36ee7188739b659e46ff456d1bc68abea45
wdcxc <904342189@qq.com>
2016-12-08 18:05:53 +0800

    init

---
+++ client.py
@@ -0,0 +1,53 @@
+#coding:utf-8
+
+import socket
+import threading
+
+class Client(object):
+    def __init__(self,serverHost='10.1.102.157',serverPort=6666):
+        self.__clientHost = socket.gethostbyname(socket.gethostname())
+        self.clientPort = None
+        self.serverHost = serverHost
+        self.serverPort = serverPort
+        self.__userName = None
+
+    def run(self):
+        print "client ip:",self.__clientHost
+        clientSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        clientSock.connect((self.serverHost,self.serverPort))
+        if self.createConn(clientSock):
+            while True:
+                if self.__userName is None:
+                    self.__userName = raw_input("enter your username:")
+                    sendData = "0002#"+self.__userName
+                else:
+                    msg = raw_input()
+                    sendData = "0003#"+msg
+                clientSock.sendall(sendData)
+            clientSock.close()
+
+    def createConn(self,clientSock):
+        invCode = raw_input("show your invcode:")
+        sendData = "0001#" + invCode
+        clientSock.sendall(sendData)
+        recvData = clientSock.recv(1024)
+        print "server:"+recvData
+        if recvData != "welcome":
+            clientSock.close()
+            return False
+        else:
+            threading.Thread(target=self.recvMsg, args=(clientSock,)).start()
+            return True
+
+    def recvMsg(self,clientSock):
+        while True:
+            recvData = clientSock.recv(1024)
+            if recvData:
+                if recvData.find("#") != -1:
+                    recvData = recvData.split("#")
+                    print "\n"+recvData[1]+":"+recvData[2]
+                else:
+                    print recvData
+
+if __name__ == "__main__":
+    Client().run()
---
+++ server.py
@@ -0,0 +1,86 @@
+#coding:utf-8
+import socket
+import threading
+sendMsgLock = threading.Semaphore(value=1)
+
+class Server(object):
+    def __init__(self,invCode):
+        self.__serverHost = '10.1.102.157'
+        self.__serverPort = 6666
+        self.conns  = 5
+        self.whiteIps = []
+        self.blockIps = []
+        self.__msgQueue = []
+        self.__aliveConns = {}
+        self.__invCode = invCode
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        sock.bind((self.__serverHost,self.__serverPort))
+        sock.listen(self.conns)
+        print "[init]server startup"
+        threading.Thread(target=self.sendMsg).start()
+        while True:
+            conn,clientAddr = sock.accept()
+            print "[+]new connection:",clientAddr
+            aliveConnKey = clientAddr[0]+":"+str(clientAddr[1])
+            if aliveConnKey not in self.__aliveConns:
+                if self.validConn(conn,aliveConnKey):
+                    threading.Thread(target=self.connClient,args=(conn,aliveConnKey,)).start()
+        sock.close()
+
+    def validConn(self,conn,aliveConnKey):
+        recvData = conn.recv(1024)
+        print "recv:" + aliveConnKey + ":" +recvData
+        recvData = recvData.split("#")
+        msgCode = recvData[0]
+        if msgCode == "0001":
+            if recvData[1] != self.__invCode:
+                conn.sendall("sorry,invcode error")
+                conn.close()
+                print "refuse conn:" + aliveConnKey
+                return False
+            else:
+                self.__aliveConns[aliveConnKey] = {"conn": conn}
+                self.showAliveConns()
+                conn.sendall("welcome")
+                print "accept conn:" + aliveConnKey
+                return True
+
+    def connClient(self,conn,aliveConnKey):
+        while True:
+            try:
+                recvData = conn.recv(1024)
+                print "recv:" + aliveConnKey + ":" + recvData
+                recvData = recvData.split("#")
+                msgCode = recvData[0]
+                if msgCode == "0002":
+                    self.__aliveConns[aliveConnKey]['uname'] = recvData[1]
+                    conn.sendall("setup uname success.")
+                elif msgCode == "0003":
+                    sendMsgLock.acquire()
+                    self.__msgQueue.append(aliveConnKey+"#"+self.__aliveConns[aliveConnKey]['uname']+"#"+recvData[1])
+                    sendMsgLock.release()
+                else:
+                    print "msg code error:"
+            except Exception,e:
+                print e
+                conn.close()
+                del self.__aliveConns[aliveConnKey]
+                print "[-]"+aliveConnKey+" disconnect"
+                self.showAliveConns()
+                break
+
+    def sendMsg(self):
+        while True:
+            if self.__aliveConns and self.__msgQueue:
+                for aAddr,aConn in self.__aliveConns.items():
+                    aConn['conn'].sendall(self.__msgQueue[0])
+                del self.__msgQueue[0]
+
+    def showAliveConns(self):
+        print "alive connection(%s):%s"%(len(self.__aliveConns.keys()),self.__aliveConns.keys())
+
+if __name__ == "__main__":
+    invCode = raw_input("setup server invcode:")
+    Server(invCode).run()
